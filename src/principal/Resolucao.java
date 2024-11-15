ae679ac7d82b9e75cb3d556c770b3ec6fd59e97c
Kleber Vales <ksvales@outlook.pt>
2024-11-15 09:17:29 -0300

    Enviando cod

--- src/principal/Resolucao.java
+++ src/principal/Resolucao.java
@@ -1,5 +1,35 @@
 package principal;
 
-public class Resolucao {
+import java.util.Scanner;
 
+class Sports {
+	String getName() {
+		return "Generic Sports";
+	}
+
+	void getNumberOfTeamMembers() {
+		System.out.println("Each team has n players in " + getName());
+	}
+}
+
+class Soccer extends Sports {
+	String getName() {
+		return "Soccer Class";
+	}
+
+	void getNumberOfTeamMembers() {
+		System.out.println("Each team has 11 players in " + getName());
+	}
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Sports s = new Sports();
+		Soccer s1 = new Soccer();
+		System.out.println(s.getName());
+		s.getNumberOfTeamMembers();
+		System.out.println(s1.getName());
+		s1.getNumberOfTeamMembers();
+
+	}
 }
